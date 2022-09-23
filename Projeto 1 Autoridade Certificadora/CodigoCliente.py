0f88e9d310219c84f3b5deaa261b3be239a0647d
Luis Felipe M <luismota140@ymail.com>
2022-09-23 13:21:46 -0300

    Atualização 23/09/2022

--- Projeto 1 Autoridade Certificadora/CodigoCliente.py
+++ Projeto 1 Autoridade Certificadora/CodigoCliente.py
@@ -1,1 +1,24 @@
 #Código do cliente
+
+#Código TCP
+import random
+from socket import socket, AF_INET, SOCK_STREAM
+#Passo 1: Criando o socket.
+mClientSocket = socket(AF_INET, SOCK_STREAM)
+#Passo 2: Transformando o socket criado num socket de um cliente,
+#ou seja, colacando para realizar solicitações.
+mClientSocket.connect(('localhost', 1235))
+
+data = mClientSocket.recv(2048)
+ChavePrivada = data.decode()
+print(f"Sua chave privada é de: {ChavePrivada}")
+
+while True:
+    # Este loop foi criado apenas para que o cliente conseguisse enviar múltiplas solicitações
+    message = input('>>')
+    #Enviando a mensagem pelo socket criado.
+    mClientSocket.send(message.encode())
+    #Recebendo as respostas do servidor.
+    data = mClientSocket.recv(2048)
+    reply = data.decode()
+    print(f'Resposta recebida:{reply}')
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Projeto: Programação utilizando sockets.
-Este projeto é o quarto componente da avaliação da disciplina.
+
+
+## Relatórios
+- Clique [AQUI](https://docs.google.com/document/d/1z2wJ6Upt8mXs8obN7JQXKzygnIXKEOsTbVdQIQTDtj8/edit) para acessar o relatório do projeto 1
+- Clique [AQUI](https://docs.google.com/document/d/1gCA_o-_P5Y3wnS7YbWYmtmfY0VtBNbZOsCIj1fLr0qA/edit) para acessar o relatório do projeto 2
+
+## Status:
+* Projeto 1: Em Desenvolvimento
+* Projeto 2: Em Desenvolvimento
+
+-----
 
 ### Grupo: Luis Felipe Mota, Paulo Vitor, Luiz Barbosa, Wilson Freitas
