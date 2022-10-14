6befd939d4bbe6d731c1433507bb067cf67b9a8c
Vittorr <paulobjalves@hotmail.com>
2022-10-13 22:41:33 -0300

    Começo do projeto 2

---
+++ Projeto 2 Servidor Web/teste_cliente.py
@@ -0,0 +1,92 @@
+#Código do cliente usando TCP
+
+import random
+from socket import socket, AF_INET, SOCK_STREAM
+import raizesPrimitivas2
+import cryptocode
+import rsa
+
+# Criando o socket
+Socket_Client = socket(AF_INET, SOCK_STREAM)
+
+# Colocando o socket para realizar solicitações
+Socket_Client.connect(('localhost', 13524))
+
+(pubKey, privKey) = rsa.newkeys(512)
+
+# Criando chaves primas e a base ou seja as chaves "p" e "g"
+p = random.randint(0, 999)
+while raizesPrimitivas2.isPrime(p) == False:
+    p = random.randint(0, 999)
+
+g = raizesPrimitivas2.findPrimitive(p)
+
+
+# Enviando as chaves padrões para o servidor
+Socket_Client.send((f'{p} {g}').encode())
+
+
+# Recebendo resposta do servidor
+resp1 = Socket_Client.recv(2048).decode()
+# print(resp1)
+
+
+# Gerando chave do cliente (chave A) e enviando para o servidor
+valueA = random.randint(0, 999)
+keyA = str((g ** valueA) % p)
+Socket_Client.send(keyA.encode())
+
+
+# Recebendo chave do servidor (chave B)
+ChaveServidor = int(Socket_Client.recv(2048).decode())
+# print(f'A chave do servidor recebida foi {ChaveServidor}')
+
+
+# Gerando as chaves compartilhadas para ambos os lados
+secretKey = (ChaveServidor ** valueA) % p
+print(f'>> A chave compartilhada: {secretKey}')
+
+# Mandando a chave compartilhada para o servidor para que tenha noção que é igual
+Socket_Client.send(str(secretKey).encode())
+
+# Recebendo chaves do servidor para comprovar que é igual
+resposta = Socket_Client.recv(2048)
+sameKey = resposta.decode()
+
+# Recebendo o identificador
+data = Socket_Client.recv(2048)
+ident = data.decode()
+print(ident)
+
+# Loop para o cliente enviar inúmeras solicitações/mensagens/arquivos
+while True:
+    # Mensagem que o cliente deseja enviar
+    message = input('>> Escreva a mensagem: ')
+    message = message + f"|{sameKey}"
+
+    # Mensagem Criptografada
+    EncryptedMessage = cryptocode.encrypt(message, str(secretKey))
+
+    # Enviando a mensagem pelo socket criado
+    Socket_Client.send(EncryptedMessage.encode())
+
+    # Fazendo assinatura digital e enviando dados para o servidor verificar
+    sign = rsa.sign(message.encode(), privKey, 'SHA-1')
+
+    # print(sign)
+
+    # verification = rsa.verify(message.encode(), sign, pubKey)
+
+    # print(verification) -----> pq funciona no cliente e no servidor não???
+
+    Socket_Client.send((f'{sign}.....{pubKey}').encode())
+
+
+    # Recebendo as respostas do servidor
+    data = Socket_Client.recv(2048)
+    
+    # Decodificando a mensagem para mostrar a mensagem recebida
+    reply = data.decode()
+    print(f'>> {reply}')
+    print()
+
---
+++ Projeto 2 Servidor Web/teste_servidor.py
@@ -0,0 +1,132 @@
+#Código do Servidor
+
+import random
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+from typing import List
+import cryptocode
+import uuid
+import rsa
+
+def HandleRequest(Socket_Client, mClientAddr):
+    # Recebendo chaves comuns "p" e "g" geradas pelo cliente
+    dados = Socket_Client.recv(2048).decode()
+
+    # Armazenando chaves separadas em duas variáveis sendo primo = "p" e raiz = "g"
+    chaves = dados.split()
+    primo = int(chaves[0])
+    raiz = int(chaves[1])
+    # print(f'O número primo recebido pelo cliente é {primo} e a raiz primitiva é {raiz}')
+
+    # Confirmando o recebimento das chaves
+    resposta = 'Chaves recebidas'
+    Socket_Client.send(resposta.encode())
+
+    # Gerando a chave do servidor (chave B) e enviando para o cliente
+    valueB = random.randint(0,999)
+    keyB = str((raiz ** valueB) % primo)
+    Socket_Client.send(keyB.encode())
+
+
+    # Recebendo chave do cliente (chave A)
+    chaveCliente = int(Socket_Client.recv(2048).decode())   
+    # print(f'A chave do cliente recebida foi {chaveCliente}')
+
+
+    # Gerando chave compartilhada entre o servidor e o cliente
+    secretKey = (chaveCliente ** valueB) % primo
+    print(f'>> A chave compartilhada: {secretKey}')
+
+    # Enviando chave para o cliente para que checar se é igual
+    Socket_Client.send(str(secretKey).encode())
+
+    # Recebendo chave compartilhada do cliente para confirmar se é igual
+    respostaCliente = Socket_Client.recv(2048)
+    sameKey = respostaCliente.decode()
+
+    CodCliente = str(uuid.uuid4())
+    while CodCliente in IDclient:
+        CodCliente = str(uuid.uuid4())
+
+
+    escrita = open ('dicionario2.txt','a')
+
+    IDclient[CodCliente] = sameKey
+    print(IDclient)
+
+    with escrita:
+        escrita.write(f'{CodCliente} {sameKey} \n')
+
+    msgident = f'>> Seu identificador é: {CodCliente}'
+    Socket_Client.send(msgident.encode())
+
+    while IDclient[CodCliente] == sameKey:
+        # Loop para O servidor receber diversas requisições de um mesmo cliente sem criar uma nova conexão
+        # print('Esperando o próximo pacote ...')
+
+        # Recebendo os dados do cliente e decodificando para mostrar o que foi recebido por ele
+        # A mensagem recebida aqui está criptografada pelo cliente
+        data = Socket_Client.recv(2048)
+        # print(f'>> A mensagem criptografada foi: {req}')
+        req = data.decode()
+        print("--- Uma Mensagem foi Recebida ---")
+        print(f'>> A mensagem criptografada foi: {req}')
+        # Descriptografando a mensagem
+        DecryptedMessage = cryptocode.decrypt(req, str(secretKey))
+
+        #Verificação da Identificação do cliente com o segredo compartilhado
+        ListDecrypted = DecryptedMessage.split("|")
+        DecryptedMessage = ListDecrypted[0]
+        sameKey = ListDecrypted[1]
+        print(f'>> Mensagem descriptografada: {DecryptedMessage}')
+        print(f'>> Mensagem Recebida do cliente {CodCliente}')
+        print()
+
+        
+        # Recebendo os dados enviados pelo cliente para verificar a assinatura digital
+        dadosRSA = Socket_Client.recv(2048).decode()
+        dados_RSA = dadosRSA.split('.....')
+
+        assinatura = dados_RSA[0]
+        pubKey = dados_RSA[1]
+
+        # print(pubKey)
+
+        # verification = rsa.verify(DecryptedMessage.encode(), assinatura, rsa.PublicKey(pubKey[0], pubKey[1]))
+
+        # print(verification) --> não funcionando falta verificar....
+
+
+        # Servidor mandando uma resposta para o cliente mostrando que o servidor está ativo e funcionando
+        rep = '>> Mensagem enviada com sucesso...'
+        Socket_Client.send(rep.encode())
+
+# Criação do socket do servidor
+Socket_Server = socket(AF_INET, SOCK_STREAM)
+print('>> Servidor criado...')
+
+# Vinculando o socket do servidor a um endereço específico
+Socket_Server.bind(('127.0.0.1', 13524))
+
+# Colocando o servidor para escutar as solicitações de conexão dos inúmeros clientes
+Socket_Server.listen()
+
+IDclient = {}
+
+
+leitura = open ('dicionario2.txt','r')
+
+with leitura:
+    for linha in leitura:
+        chave, conteudo = linha.split()
+        IDclient[chave] = conteudo
+
+contador = 0
+
+while True:
+    # Loop para o servidor conseguir se conectar com vários clientes e colocando-o para aceitar as solicitações de conexão
+    Socket_Client, clientAddr =  Socket_Server.accept()
+    print(f'>> O servidor aceitou a conexao do cliente: {clientAddr}')
+    
+    Thread(target=HandleRequest, args=(Socket_Client, clientAddr)).start()
+
